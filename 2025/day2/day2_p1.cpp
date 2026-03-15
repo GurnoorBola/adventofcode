6360b0ef9026658ef4d7d1d20e595acc69e5753c
noor <gurnoor.bola@gmail.com>
2026-03-15 16:08:07 -0700

    cleaned up solution

--- 2025/day2/day2_p1.cpp
+++ 2025/day2/day2_p1.cpp
@@ -1,90 +1,70 @@
 #include <iostream>
-#include <cmath>
 
-constexpr long long pow(long long a, long long x)
-{
-    long long res{1};
-    for (long long i{0}; i < x; ++i)
-        res *= a;
+using ll = long long;
+
+constexpr ll pow(ll a, int x) {
+    ll res{1};
+    for (int i{0}; i < x; ++i) res *= a;
     return res;
 }
 
-constexpr long long numDigits(long long a)
-{
-    long long max{a};
-    long long digits{0};
-    while (max > 0)
-    {
+constexpr int numDigits(ll a) {
+    ll max{a};
+    int digits{0};
+    while (max > 0) {
         max /= 10;
         digits += 1;
     }
     return digits;
 }
 
-constexpr long long sumInvalid(long long lower, long long upper)
-{
-    long long res{0};
+constexpr ll sumInvalid(ll lower, ll upper) {
+    ll res{0};
 
-    //ex: 11 since 1 and 1 is repeated
-    //ex: 123123 since 1 and 1 is repeated
+    // ex: 11 since 1 and 1 is repeated
+    // ex: 123123 since 1 and 1 is repeated
     //
-    //idea: to check if number is invalid just find the number of digitis it has
-    //then find if num / 10^(digits/2) == num % 10^(digits/2)
-    //
-    //if number of digits is odd we treat as if there is invisible 0 in front 
-    //so we check with (digits+1)/2 instead
-    
-    long long digits{numDigits(lower)};
-    long long max{pow(10, digits)};
+    // idea: to check if number is invalid just find the number of digits it has
+    // then find if num / 10^(digits/2) == num % 10^(digits/2)
 
-    long long start{lower};
-    if (digits % 2 == 1)
-    {
+    int digits{numDigits(lower)};
+    ll max{pow(10, digits)};
+
+    ll start{lower};
+    if (digits % 2 == 1) {
         start = max;
         max *= 10;
         digits += 1;
     }
 
-    long long divisor{pow(10, digits/2)};
-    
-    for (long long num{start}; num <= upper; ++num)
-    {
-        if (num >= max)
-        {
+    ll divisor{pow(10, digits / 2)};
+
+    for (ll num{start}; num <= upper; ++num) {
+        if (num >= max) {
             digits += 1;
             max *= 10;
-            if (digits % 2 == 1)
-            {
+            if (digits % 2 == 1) {
                 max *= 10;
                 num *= 10;
                 digits += 1;
             }
-                
         }
-        long long left{num / divisor};
-        long long right{num % divisor};
-        
-        // std::cout << "Parts: " << left << " " << right << "\n";
+        ll left{num / divisor};
+        ll right{num % divisor};
 
-        res += ((left == right) ? num: 0);
+        res += ((left == right) ? num : 0);
     }
 
     return res;
 }
 
-
-int main () {
-
-    long long res{0};
-    long long lower{};
-    long long upper{};
+int main() {
+    ll res{0};
+    ll lower{};
+    ll upper{};
     char garb{};
 
-    while(std::cin >> lower >> garb >> upper >> garb)
-    {
-
-        res += sumInvalid(lower, upper);
-    }
+    while (std::cin >> lower >> garb >> upper >> garb) { res += sumInvalid(lower, upper); }
 
     std::cout << "Result: " << res << "\n";
 
